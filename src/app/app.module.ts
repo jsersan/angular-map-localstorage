dbcdb898a66eba2607ab40c635c7dfb3e576ed93
Txema Serrano <jsersan@gmail.com>
2024-12-30 19:37:42 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { MaterialModule } from './material.module';
+import { MapaComponent } from './components/mapa/mapa.component';
+
+import { AgmCoreModule } from '@agm/core';
+import { MapaEditarComponent } from './components/mapa/mapa-editar.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+@NgModule({
+  entryComponents:[
+    MapaEditarComponent
+  ],
+  declarations: [
+    AppComponent,
+    MapaComponent,
+    MapaEditarComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyARPYo6hXKPHDSGN9Das5FGK2EF1HTeNps'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
